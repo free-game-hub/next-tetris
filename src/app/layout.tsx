b51f0cd2bc8e3c65498ed22d4da361a4e6e3d218
gangbo <daigangbo@gmail.com>
2025-01-18 17:00:55 +0800

    #

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import {Geist, Geist_Mono, Staatliches} from "next/font/google";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import StatCounterScript from "@/components/StatCounterScript";
 
